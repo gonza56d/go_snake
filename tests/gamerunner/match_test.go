3c24dffe5c9e682779ebac4d5f9df89cb1a685f5
gonza56d <gonza56d@gmail.com>
2024-12-06 17:00:38 -0300

    Test game over

--- tests/gamerunner/match_test.go
+++ tests/gamerunner/match_test.go
@@ -151,3 +151,75 @@ func TestMakeMoveAndEatFood(t *testing.T) {
 	}
 }
 
+func TestMakeMoveAndGameOver(t *testing.T) {
+	tests := []struct{
+		snakeMovement gamecomponents.SnakeMovement
+		match *gamerunner.Match
+	}{
+		{
+			gamecomponents.Up,
+			&gamerunner.Match{
+				GameMap: gamecomponents.Map{XSize: 30, YSize: 30, FoodAt: gamecomponents.Food{XAt: 5, YAt: 6}},
+				Score: uint64(6),
+				Snake: gamecomponents.Snake{
+					gamecomponents.Location{XAt: 5, YAt: 30},
+					gamecomponents.Location{XAt: 5, YAt: 29},
+					gamecomponents.Location{XAt: 5, YAt: 28},
+					gamecomponents.Location{XAt: 5, YAt: 27},
+				},
+			},
+		},
+		{
+			gamecomponents.Right,
+			&gamerunner.Match{
+				GameMap: gamecomponents.Map{XSize: 30, YSize: 30, FoodAt: gamecomponents.Food{XAt: 6, YAt: 5}},
+				Score: uint64(6),
+				Snake: gamecomponents.Snake{
+					gamecomponents.Location{XAt: 30, YAt: 5},
+					gamecomponents.Location{XAt: 29, YAt: 5},
+					gamecomponents.Location{XAt: 28, YAt: 5},
+					gamecomponents.Location{XAt: 27, YAt: 5},
+				},
+			},
+		},
+		{
+			gamecomponents.Left,
+			&gamerunner.Match{
+				GameMap: gamecomponents.Map{XSize: 30, YSize: 30, FoodAt: gamecomponents.Food{XAt: 4, YAt: 5}},
+				Score: uint64(6),
+				Snake: gamecomponents.Snake{
+					gamecomponents.Location{XAt: 0, YAt: 5},
+					gamecomponents.Location{XAt: 1, YAt: 5},
+					gamecomponents.Location{XAt: 2, YAt: 5},
+					gamecomponents.Location{XAt: 3, YAt: 5},
+				},
+			},
+		},
+		{
+			gamecomponents.Down,
+			&gamerunner.Match{
+				GameMap: gamecomponents.Map{XSize: 30, YSize: 30, FoodAt: gamecomponents.Food{XAt: 5, YAt: 4}},
+				Score: uint64(6),
+				Snake: gamecomponents.Snake{
+					gamecomponents.Location{XAt: 5, YAt: 0},
+					gamecomponents.Location{XAt: 5, YAt: 1},
+					gamecomponents.Location{XAt: 5, YAt: 2},
+					gamecomponents.Location{XAt: 5, YAt: 3},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		scoreBeforeMoving := test.match.Score
+		var gameOver bool = test.match.MakeMove(test.snakeMovement)
+		if !gameOver {
+			t.Error("Snake should have crashed after MakeMove and Game Over should happen.")
+		}
+		if scoreBeforeMoving != test.match.Score {
+			t.Errorf("test.match.Score (%d) should be equal than scoreBeforeMoving (%d) " + 
+				"after MakeMove(...) call.", test.match.Score, scoreBeforeMoving)
+		}
+	}
+}
+
