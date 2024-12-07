fa7a191599a395d1fce0cf87b8154f5cac88f8ad
gonza56d <gonza56d@gmail.com>
2024-12-07 17:21:22 -0300

    Building screen

--- cmd/go_snake/main.go
+++ cmd/go_snake/main.go
@@ -3,34 +3,72 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/eiannone/keyboard"
+	"github.com/gonza56d/go_snake/internal/gamecomponents"
+	"github.com/gonza56d/go_snake/internal/gamerunner"
 )
 
 func main() {
+	match := gamerunner.NewMatch(3)
+	nextMovement := gamecomponents.Up
+	movementsMap := map[rune]gamecomponents.SnakeMovement{
+		'w': gamecomponents.Up,
+		'W': gamecomponents.Up,
+		'a': gamecomponents.Left,
+		'A': gamecomponents.Left,
+		's': gamecomponents.Down,
+		'S': gamecomponents.Down,
+		'd': gamecomponents.Right,
+		'D': gamecomponents.Right,
+	}
 	// Initialize the keyboard listener
 	if err := keyboard.Open(); err != nil {
 		log.Fatal(err)
 	}
 	defer keyboard.Close()
 
-	fmt.Println("Press any key to see its value. Press ESC to quit.")
+	movementChannel := make(chan gamecomponents.SnakeMovement, 1)
+
+	go func() {
+		ticket := time.NewTicker(100 * time.Millisecond)
+		defer ticket.Stop()
+
+		movement := nextMovement
+		for {
+			select {
+			case <-ticker.C:
+				match.MakeMove(movement)
+				var screenBuffer string
+				for x := range match.GameMap.XSize {
+					for y := range match.GameMap.YSize {
+						if x == 0 || x == len(match.GameMap.XSize) || y == 0 || y == len(match.GameMap.YSize) {
+							
+						}
+					}
+				}
+			case newMovement := <-movementChannel:
+				movement = newMovement
+			}
+		}
+	}
 
 	for {
 		char, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			// Handle the error but don't exit the program
-			fmt.Println("Error reading key:", err)
-			continue // Skip this iteration and wait for the next key press
-		}
-
-		// Display the key pressed
-		fmt.Printf("You pressed: char=%q, key=%v\n", char, key)
-
 		// Exit the program when ESC is pressed
-		if key == keyboard.KeyEsc {
+		if err {
+			fmt.Printf("Error happened while pressing key %v", key)
+		}
+		if char == '\x00' {
 			fmt.Println("Exiting...")
 			break
+		}
+		if movement, exists := movementsMap[char]; exists {
+			select {
+			case movementChannel <- movement:
+			default:
+			}
 		}
 	}
 }
