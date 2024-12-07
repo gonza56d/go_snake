29d963162b5d914ad5bd2aedaacc3b5cd6d39297
gonza56d <gonza56d@gmail.com>
2024-12-07 20:27:10 -0300

    making screen work

--- cmd/go_snake/main.go
+++ cmd/go_snake/main.go
@@ -23,6 +23,28 @@ func main() {
 		'd': gamecomponents.Right,
 		'D': gamecomponents.Right,
 	}
+	// initial draw for screen (walls)
+	maxX := match.GameMap.XSize + 2
+	maxY := match.GameMap.YSize + 2
+	screen := make([]byte, 7482, 7482)
+	fmt.Println((maxX + 1) ^ maxY)
+	for x := range(maxX+1) {
+		for y := range(maxY) {
+			if x == maxX + 1 {
+				screen[x+y] = '\n' // line break (next y)
+			} else if (x==0 && y==0) || (x==0 && y==maxY) || (x==maxX && y==0) || (x==maxX && y==maxY) {
+				screen[x+y] = 'X' // corners
+			} else if x == 0 || x == match.GameMap.XSize + 1 { 
+				screen[x+y] = '-' // bottom/top walls
+			} else if y == 0 || y == match.GameMap.YSize + 1 {
+				screen[x+y] = 'l' // side walls
+			} else {
+				screen[x+y] = ' ' // blank spaces for snake & food
+			}
+		}
+	}
+	fmt.Println(string(screen))
+
 	// Initialize the keyboard listener
 	if err := keyboard.Open(); err != nil {
 		log.Fatal(err)
@@ -32,32 +54,24 @@ func main() {
 	movementChannel := make(chan gamecomponents.SnakeMovement, 1)
 
 	go func() {
-		ticket := time.NewTicker(100 * time.Millisecond)
-		defer ticket.Stop()
+		ticker := time.NewTicker(100 * time.Millisecond)
+		defer ticker.Stop()
 
 		movement := nextMovement
 		for {
 			select {
 			case <-ticker.C:
 				match.MakeMove(movement)
-				var screenBuffer string
-				for x := range match.GameMap.XSize {
-					for y := range match.GameMap.YSize {
-						if x == 0 || x == len(match.GameMap.XSize) || y == 0 || y == len(match.GameMap.YSize) {
-							
-						}
-					}
-				}
 			case newMovement := <-movementChannel:
 				movement = newMovement
 			}
 		}
-	}
+	}()
 
 	for {
 		char, key, err := keyboard.GetSingleKey()
 		// Exit the program when ESC is pressed
-		if err {
+		if err != nil {
 			fmt.Printf("Error happened while pressing key %v", key)
 		}
 		if char == '\x00' {
